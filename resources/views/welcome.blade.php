b1e00ade8988ea3a7047b8eba7215f9460b1605b
will1227 <williamcg1227@gmail.com>
2025-05-20 20:16:37 -0300

    commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
     <p>seja bem vindo ao sistema</p>
 
     <ul>
-        <li><a href="{{ url('/types') }}">Cadastro de tipos</a></li>
-        <li><a href="{{ url('/products') }}">Cadastro de produtos</a></li>
+        <li><a href="{{ url('/types') }}">Manutenção de tipos</a></li>
+        <li><a href="{{ url('/products') }}">Manutenção de produtos</a></li>
     </ul>
 </body>
 </html>
